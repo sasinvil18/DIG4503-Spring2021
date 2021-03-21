f8ef85e42294b45a1212928226b0ab90a4ec8403
sasinvil18 <59659039+sasinvil18@users.noreply.github.com>
2021-03-21 02:53:06 -0400

    lab8

--- lab8/client/src/components/Add/index.js
+++ lab8/client/src/components/Add/index.js
@@ -3,13 +3,12 @@ import { useState } from "react";
 
 function Add(){
   const [addName, setAddName] = useState("");
-  const [added, setAdded] = useState([]);
+
 
   const putName = (name) => {
     Axios.put("http://localhost:45030/people/" + name)
     .then(response => {
       console.log(response.data);
-      setAdded(response.data.person);
     })
     .catch(error => {
       console.log("Error: " + error)
@@ -21,7 +20,6 @@ function Add(){
       <button onClick={() => {
           putName(addName);
       }}>Click to Add a Name</button>
-
     </div>
   );
 }
--- lab8/client/src/components/Search/index.js
+++ lab8/client/src/components/Search/index.js
@@ -3,13 +3,11 @@ import { useState } from "react";
 
 function Search(){
   const [searchName, setSearchName] = useState("");
-  const [searchResult, setSearchResult] = useState("");
 
   const getName = (person) => {
     Axios.get("http://localhost:45030/people/" + person)
     .then(response => {
       console.log(response.data);
-      setSearchResult(response.data.person);
     })
     .catch((error) => {
       console.log("Error: " + error);
@@ -21,7 +19,6 @@ function Search(){
       <input type="text" onChange={(event) => setSearchName(event.target.value)}></input>
 
       <button onClick={() => getName(searchName)}>Click to Search for a Name</button>
-
     </div>
   )
 }
