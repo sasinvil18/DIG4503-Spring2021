26453806077e92020cfbb64b71b2478e93babb48
sasinvil18 <59659039+sasinvil18@users.noreply.github.com>
2021-05-01 23:50:19 -0400

    portfolio2

--- portfolio2/client/src/App.js
+++ portfolio2/client/src/App.js
@@ -6,7 +6,7 @@ import UpdateFav from './components/UpdatePokemon';
 import DeleteFav from './components/DeletePokemon';
 import SearchPokemon from './components/SearchPokemon';
 
-//Search for movies using the sample data with the ability to save favorites in a separate collection based on their username or identification.
+//Building Pokemon team by connecting PokeAPI to MongoDB
 
 function App() {
 
